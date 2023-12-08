95bb85f8afa3c18b61a9fc89ab279ae72ec06fa0
sumitaggarwalhomes <sumitagg@outlook.com>
2023-12-08 13:58:24 -0500

    update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/resources/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702061461';
+const cacheName = 'chirpy-1702061891';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
